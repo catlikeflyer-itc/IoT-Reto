b673b9ad6de0edb8bb370928519c04c73cff6708
Do Hyun Nam <dollis2002@gmail.com>
2021-11-30 22:57:22 -0600

    Fixed grid

--- iot-app/src/Pages/Registros.jsx
+++ iot-app/src/Pages/Registros.jsx
@@ -3,13 +3,16 @@ import { Grid } from "@mui/material";
 import axios from "axios";
 import RegistroCard from "../Components/RegistroCard";
 import AddRegistro from "../Components/AddRegistro";
+import { Box } from "@mui/system";
 
 export default function Registros() {
   const [data, setData] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios("https://api-reto-iot.herokuapp.com/registros");
+      const result = await axios(
+        "https://api-reto-iot.herokuapp.com/registros"
+      );
 
       setData(result.data);
     };
@@ -18,19 +21,21 @@ export default function Registros() {
   }, []);
 
   return (
-      <Grid container spacing={3}>
-        <Grid item xs={12} md={4}>
-          <AddRegistro />
-        </Grid>
-        <Grid item xs={12} md={8}>
-            <Grid container spacing={3}>
-              {data.map((d) => (
-                <Grid item xs={12} md={4}>
-                  <RegistroCard data={d} key={d.id} />
-                </Grid>
-              ))}
-            </Grid>
-        </Grid>
+    <Grid container spacing={3}>
+      <Grid item xs={12} md={4}>
+        <AddRegistro />
       </Grid>
+      <Grid item xs={12} md={8}>
+        <Box m={4} display="flex" flexDirection="row">
+          <Grid container spacing={3}>
+            {data.map((d) => (
+              <Grid item xs={12} md={4}>
+                <RegistroCard data={d} key={d.id} />
+              </Grid>
+            ))}
+          </Grid>
+        </Box>
+      </Grid>
+    </Grid>
   );
 }
